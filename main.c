a927c74485b361aa405d6da45dad5cf3368879eb
Barry Wardell <barry.wardell@gmail.com>
2011-04-09 23:46:42 +0200

    Update example code.

--- main.c
+++ main.c
@@ -5,27 +5,53 @@
 int main(int argc, char* argv[])
 {
   struct coordinate x, xp, up;
-  
+
+  /* Mass and spin of the central black hole */
   const double a = 0.5;
+  const double a2  = a*a;
+  const double a3  = a2*a;
+  const double a4  = a2*a2;
   const double M = 1.0;
 
-  /* The particle's coordinate location */
-  xp.r     = 9.;
-  xp.theta = M_PI_2;
-  xp.phi   = 0.0;
-  xp.t     = 0.0;
+  /* Energy an angular momentum for an elliptic orbit between r=9 and r=11 (M=1) */
+  double e  = sqrt((-434070 + 2471*a2 + 6*sqrt(110)*a*sqrt(6237 + 162*a2 + a4))/(-474721 + 3960*a2));
+  double l  = ((261*a + a3 - 3*sqrt(110)*sqrt(6237 + 162*a2 + a4))*
+              sqrt((-434070 + 2471*a2 + 6*sqrt(110)*a*sqrt(6237 + 162*a2 + a4))/(-474721 + 3960*a2)))/(-630 + a2);
 
-  /* The field point */
-  x.theta   = M_PI_2;
-  x.t       = 0.;
+  /* 4-velocity for the elliptic orbit - choose ingoing motion */
+  double rp  = 10.0;
+  double rp2 = rp*rp;
+  double rp3 = rp2*rp;
+  double e2  = e*e;
+  double ur2 = -1 + e2 - 2*(-(l-a*e)*(l-a*e)*M/rp3 + (l*l-a2*(e2-1))/(2.*rp2) - M/rp);
+
+  up.t   = (-2*a*l*M + e*rp3 + a2*e*(2*M + rp))/(rp*a2 + rp*(-2*M + rp));
+  up.phi = (2*a*e*M - 2*l*M + l*rp)/(a2*rp - 2*M*rp2 + rp3);
+  up.r   = ur2 > 0.0 ? -sqrt(ur2) : 0.0;
+
+  xp.t = 0;
+  xp.r = rp;
+  xp.theta = M_PI_2;
+  xp.phi = 0;
+
+  /* The point where we measure the effective source */
+  x.t     = 0.0;
+  x.r     = rp;
+  x.theta = M_PI_2;
+  x.phi   = 0.0;
 
   /* Initialize the background parameters */
   effsource_init(M, a);
-  
-  /* Set particle's position and 4-velocity */
-  effsource_set_particle(&xp, &up);
 
-  for(double r=4.0; r<=14.0; r+=0.1)
+  int use_el = 1;
+  if( use_el )
+  {
+    effsource_set_particle_el(&xp, e, l, up.r);
+  } else {
+    effsource_set_particle(&xp, &up);
+  }
+
+  for(double r=8.0; r<=12.0; r+=0.1)
   {
     x.r = r;
     for(double phi=-M_PI; phi<=M_PI; phi+=0.1)
@@ -34,7 +60,9 @@ int main(int argc, char* argv[])
       x.phi     = phi;
       effsource_calc(&x, &phis, &dphis_dr, &dphis_dtheta, &dphis_dphi, &dphis_dt, &src);
       
-      printf("%g\t%g\t%g\t%g\t%g\n", x.r, x.theta, x.phi, phis, src);
+      printf("%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n",
+        x.r-xp.r, x.theta-xp.theta, x.phi-xp.phi,
+        phis, dphis_dr, dphis_dtheta, dphis_dphi, dphis_dt, src);
     }
   }
 
